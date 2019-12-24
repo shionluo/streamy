083f1263fb77c27ea5798d18ea95bab49d24b3dc
Shion <shionchill@Shions-MacBook-Pro.local>
2019-12-24 11:27:03 +0700

    Created Setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 // Import
 import React from 'react';
-import Test from 'components/Test/Test';
 
 import { GlobalStyle } from './utils/styles/global.styles';
 
@@ -10,7 +9,6 @@ const App = () => (
   <div>
     <GlobalStyle />
     <h1>Wohoooooo</h1>
-    <Test />
   </div>
 );
 
