7e8d4559e09fd5962b631c01787f1c37767c756e
Shion <shionchill@Shions-MacBook-Pro.local>
2019-12-25 20:53:56 +0700

    Created Setup

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const cors = require('cors');
 const path = require('path');
 const enforce = require('express-sslify');
 const compression = require('compression');
-const spdy = require('spdy');
-const fs = require('fs');
 
 // Setup
 const app = express();
@@ -32,18 +30,8 @@ app.get('/service-worker.js', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
 });
 
-// SSL Certificate
-const options = {
-  key: fs.readFileSync('./utils/http2/server.key'),
-  cert: fs.readFileSync('./utils/http2/server.crt'),
-  allowHTTP1: true,
-};
-
-// Serving using HTTP2
-spdy.createServer(options, app).listen(port, err => {
-  if (err) {
-    throw new Error(err);
-  }
-
-  console.log(`Listening on port: ${port}.`);
+// Serving
+app.listen(port, error => {
+  if (error) throw error;
+  console.log(`Listening on port: ${port}`);
 });
