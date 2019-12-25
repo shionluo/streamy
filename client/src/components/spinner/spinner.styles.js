3614264e50bb9bc62f2156ef2f92cc9e3f18b079
Shion <shionchill@Shions-MacBook-Pro.local>
2019-12-25 21:54:34 +0700

    Implemented default css

--- client/src/components/spinner/spinner.styles.js
+++ client/src/components/spinner/spinner.styles.js
@@ -13,9 +13,9 @@ export const SpinnerOverlay = styled.div`
 
 export const SpinnerContainer = styled.div`
   display: inline-block;
-  width: 50px;
-  height: 50px;
-  border: 3px solid rgba(195, 195, 195, 0.6);
+  width: 5rem;
+  height: 5rem;
+  border: 0.3rem solid rgba(195, 195, 195, 0.6);
   border-radius: 50%;
   border-top-color: #636767;
   animation: spin 1s ease-in-out infinite;
